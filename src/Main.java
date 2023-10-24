5ccb905597ce3d1d7884e1b157e0ebfdf03bd52e
beatriz <beatrizmajer@gmail.com>
2023-10-24 07:19:20 -0300

    projetos bons

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         // Classe Objeto    Construtor
         Turista mochileiro = new Turista("Maia");
         // Definir formato do objeto
         mochileiro.setNome("Agnes");
-        mochileiro.setCpf(Validacao.cpf("123"));
+        mochileiro.setCpf("123");
         Turista mochileira = new Turista();
         mochileira.setNome("Tomas");
+        //Atribuição indireta
+        String nome = JOptionPane.showInputDialog("Entre com o nome");
+        mochileira.setNome(nome);
+        System.out.println("O nome do objeto");
+
 
     }
 }
